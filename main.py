00b5e12152a24f8e2e42c37f09bd7c757052ba49
hutte42 <98601549+hutte42@users.noreply.github.com>
2022-07-30 15:46:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from deck.card import Card
 from game.rules import game_result
 
 
-# Goals for next time játék mehanizmus
+# Goals for next time money system, more decks, reshufle, more players
 if __name__ == '__main__':
     result = game_result(cards_dealer=[Card("red", "J"), Card("black", "1")],
                          cards_player=[Card("red", "2"), Card("black", "k")])
     print(result)
-
