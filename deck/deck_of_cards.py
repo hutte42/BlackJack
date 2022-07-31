ead232a03903917e99b87ebebd37737628c73835
hutte42 <98601549+hutte42@users.noreply.github.com>
2022-07-31 17:20:50 +0200

    number of decks

--- deck/deck_of_cards.py
+++ deck/deck_of_cards.py
@@ -5,43 +5,46 @@ from deck.card import Card
 
 # colour: hearts, diamonds, spades, clubs; symbol: 1-10, J, Q, K, A
 class Deck:
-    def __init__(self):
+    def __init__(self, num_decks=1):
  #       self.card = [Card(colour, number) for colour, number in
  #                    product(["hearts", "diamonds", "spades", "clubs"],
  #                            [str(num) for num in range(1, 11)] + ["J", "Q", "K", "A"])]
-        self.card = []
-        for symbol in range(0, 4):
-            if symbol == 0:
-                actcolour = "hearts"
-            elif symbol == 1:
-                actcolour = "diamonds"
-            elif symbol == 2:
-                actcolour = "clubs"
-            else:
-                actcolour = "spades"
-            for number in range(2, 15):
-                if number < 11:
-                    self.card.append(Card(actcolour, str(number)))
+        self.cards = []
+        for _ in range(0, num_decks):
+            for symbol in range(0, 4):
+                if symbol == 0:
+                    actcolour = "hearts"
+                elif symbol == 1:
+                    actcolour = "diamonds"
+                elif symbol == 2:
+                    actcolour = "clubs"
                 else:
-                    if number == 11:
-                        self.card.append(Card(actcolour, "J"))
-                    elif number == 12:
-                        self.card.append(Card(actcolour, "Q"))
-                    elif number == 13:
-                        self.card.append(Card(actcolour, "K"))
+                    actcolour = "spades"
+                for number in range(2, 15):
+                    if number < 11:
+                        self.cards.append(Card(actcolour, str(number)))
                     else:
-                        self.card.append(Card(actcolour, "A"))
+                        if number == 11:
+                            self.cards.append(Card(actcolour, "J"))
+                        elif number == 12:
+                            self.cards.append(Card(actcolour, "Q"))
+                        elif number == 13:
+                            self.cards.append(Card(actcolour, "K"))
+                        else:
+                            self.cards.append(Card(actcolour, "A"))
 
     def draw_card(self):
-        card_index = random.randrange(0, len(self.card))
-        return self.card.pop(card_index)
+        card_index = random.randrange(0, len(self.cards))
+        return self.cards.pop(card_index)
 
+    def get_num_cards_left(self):
+        return len(self.cards)
 
 if __name__ == "__main__":
     d = Deck()
     for i in range(0, 56):
         print(d.draw_card())
-        print(f"{len(d.card)} cards left in the deck")
-    if len(d.card) != 0:
-        print(f"NOT GOOD DECK STILL HAS {len(d.card)} cards left after drawing 56 cards!!")
+        print(f"{len(d.cards)} cards left in the deck")
+    if len(d.cards) != 0:
+        print(f"NOT GOOD DECK STILL HAS {len(d.cards)} cards left after drawing 56 cards!!")
     a = 6
